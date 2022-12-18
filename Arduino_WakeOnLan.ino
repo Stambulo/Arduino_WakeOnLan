b26fd425ddfe0086e1e90b57910adb82dfd6551b
Stambulo <stambulo@yandex.ru>
2022-12-18 21:33:53 +0200

    web_interface_init

--- Arduino_WakeOnLan.ino
+++ Arduino_WakeOnLan.ino
@@ -3,14 +3,13 @@
 
 
 byte mac[] = { 0x00, 0x00, 0x0B, 0xAA, 0xBB, 0xCC };
-// IPAddress ip(192, 168, 1, 25);
+String readString;
 
 EthernetServer server(80);
 EthernetClient clients[8];
 
 
 void setup() {
-  // Ethernet.begin(mac, ip);
   Ethernet.begin(mac);
   Serial.begin(9600);
   Serial.println("Start Ethernet");
@@ -22,15 +21,10 @@ void setup() {
 
 
 void loop() {
-  // check for any new client connecting, and say hello (before any incoming data)
   EthernetClient newClient = server.accept();
   if (newClient) {
     for (byte i = 0; i < 8; i++) {
       if (!clients[i]) {
-        newClient.print("Hello, client number: ");
-        newClient.println(i);
-        // Once we "accept", the client is no longer tracked by EthernetServer
-        // so we must store it into our list of clients
         clients[i] = newClient;
         break;
       }
@@ -40,9 +34,16 @@ void loop() {
   // check for incoming data from all clients
   for (byte i = 0; i < 8; i++) {
     while (clients[i] && clients[i].available() > 0) {
-      // read incoming data from the client
-      Serial.write(clients[i].read());
+      char c = clients[i].read();
+      if (readString.length() < 100) {
+        readString += c;
+      }
+      if (c == 0x0D) {
+        checkRequest(readString);
+        sendResponse(clients[i]);
+      }
     }
+    readString = "";
   }
 
   // stop any clients which disconnect
@@ -51,4 +52,36 @@ void loop() {
       clients[i].stop();
     }
   }
+}
+
+void checkRequest(String request) {
+  if (readString.indexOf("?MAINPC") > -1) {
+    Serial.println(">>> Main PC starts !");
+  } else {
+    if (readString.indexOf("?MINIPC") > -1) {
+      Serial.println(">>> Mini PC starts !");
+    }
+  }
+}
+
+void sendResponse(EthernetClient client) {
+  //display the webpage
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println();
+  client.println("<HTML>");
+  client.println("<HEAD>");
+  client.println("<TITLE> ARDUINO ETHERNET</TITLE>");
+  client.println("</HEAD>");
+  client.println("<BODY>");
+  client.println("<hr>");
+  client.println("<H1 style=\"color:green;text-align:center\">ARDUINO WAKE ON LAN SERVER</H1>");
+  client.println("<hr>");
+  client.println("<br>");
+  client.println("<H2 style=\"text-align:center\"><a href=\"/?MINIPC\"\">Turn On MiniPC</a><br></H2>");
+  client.println("<H2 style=\"text-align:center\"><a href=\"/?MAINPC\"\">Turn On MainPC</a><br></H2>");
+  client.println("<br>");
+  client.println("</BODY>");
+  client.println("</HTML>");
+  client.stop();
 }
